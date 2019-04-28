e88e9e988e6a52ad59061f7cb1fa0e2c4340d52a
Will Crichton <wcrichto@cs.stanford.edu>
2019-04-28 15:47:45 -0700

    Bump to 0.1.10 for parity with vgrid

--- vgrid_jupyter/_version.py
+++ vgrid_jupyter/_version.py
@@ -4,5 +4,5 @@
 # Copyright (c) Will Crichton.
 # Distributed under the terms of the Modified BSD License.
 
-version_info = (0, 1, 9)
+version_info = (0, 1, 10)
 __version__ = ".".join(map(str, version_info))
