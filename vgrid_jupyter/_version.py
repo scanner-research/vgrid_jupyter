4d1efe34fecee0aa74b9943432168b802b3d1588
Will Crichton <wcrichto@cs.stanford.edu>
2019-07-18 22:14:17 -0700

    Bump to 0.1.13

--- vgrid_jupyter/_version.py
+++ vgrid_jupyter/_version.py
@@ -4,5 +4,5 @@
 # Copyright (c) Will Crichton.
 # Distributed under the terms of the Modified BSD License.
 
-version_info = (0, 1, 12)
+version_info = (0, 1, 13)
 __version__ = ".".join(map(str, version_info))
